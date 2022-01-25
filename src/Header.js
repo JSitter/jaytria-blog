c7fed8dc941b3e99fe26c1eb5b2ef226a3dde5d7
Justin Sitter <justin@jaytria.com>
2022-01-25 14:56:38 -0800

    Use Header styles

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,14 @@
+import './css/Header.css';
+
 function Header() {
     return (
         <header>
-            <h1>Jaytria Development Blog</h1>
+            <div class="header-background tilt">&nbsp</div>
+            <div class="header-background-stripe tilt">&nbsp</div>
+            <div class="website-heading">
+                <h1>Jaytria</h1>
+                <h2>Web Development</h2>
+            </div>
         </header>
     );
 }
