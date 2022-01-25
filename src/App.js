6e505f5d08353269cf938ddb9140b4fa40ffa1b5
Justin Sitter <justin@jaytria.com>
2022-01-25 14:18:42 -0800

    Add Posts component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
+import Posts from './Posts.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Posts />
     </div>
   );
 }
--- src/Posts.js
+++ src/Posts.js
@@ -1,7 +1,34 @@
-function Posts() {
+import { useEffect, useState } from 'react';
+import { fetchJSON } from './helpers.js';
+
+const Placeholder = () => {
     return (
         <article>
-            Posta
+            Loading ...
+        </article>
+    );
+}
+
+const Post = (response) => {
+    console.log(response.data.id);
+    return (
+        <article>
+            <h2 dangerouslySetInnerHTML={{ __html: response.data.title.rendered }}></h2>
+            <section dangerouslySetInnerHTML={{ __html: response.data.content.rendered }}></section>
+        </article >
+    );
+}
+
+function Posts() {
+    useEffect(() => {
+        fetchJSON('posts').then(json => setMainPost(json[0]));
+    }, []);
+
+    const [mainPost, setMainPost] = useState(false);
+
+    return (
+        <article>
+            {mainPost ? <Post data={mainPost} /> : <Placeholder />}
         </article>
     );
 }
