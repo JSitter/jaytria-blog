80b4fcf0a1518d2853e34910eaec8ac0e97929b5
Justin Sitter <justin@jaytria.com>
2022-01-26 15:39:53 -0800

    Fix React specific naming

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,9 @@ import './css/Header.css';
 function Header() {
     return (
         <header>
-            <div class="header-background tilt">&nbsp</div>
-            <div class="header-background-stripe tilt">&nbsp</div>
-            <div class="website-heading">
+            <div className="header-background tilt">&nbsp</div>
+            <div className="header-background-stripe tilt">&nbsp</div>
+            <div className="website-heading">
                 <h1>Jaytria</h1>
                 <h2>Web Development</h2>
             </div>
