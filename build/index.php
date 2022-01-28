b9ee1be89591722c7aa3e713c4961db3d6dcb6aa
Justin Sitter <justin@jaytria.com>
2022-01-27 22:26:03 -0800

    Thousand cors problems

--- build/index.php
+++ build/index.php
@@ -1,7 +1,7 @@
 <?php
     // Allow from any origin
     
-    header("Access-Control-Allow-Origin: blog.jaytria.com");
+    header("Access-Control-Allow-Origin: https://blog.jaytria.com");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
     
@@ -17,7 +17,5 @@
 
         exit(0);
     }
-
-    echo "You have CORS!";
 ?>
 <!doctype html><html lang="en"><head><meta charset="utf-8"/><link rel="icon" href="/favicon.ico"/><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><meta name="description" content="Web site created using create-react-app"/><link rel="apple-touch-icon" href="/logo192.png"/><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Roboto&display=swap" rel="stylesheet"><link rel="manifest" href="/manifest.json"/><title>Jaytria Development Blog</title><script defer="defer" src="/static/js/main.0ed782a2.js"></script><link href="/static/css/main.c1615a41.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div></body></html>
