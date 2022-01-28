18a16ab8742fc7b056f534ec2074031ee66a8809
Justin Sitter <justin@jaytria.com>
2022-01-27 21:08:12 -0800

    Add react router, styles, and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from './Header.js';
 import Posts from './Posts.js';
+import Footer from './Footer.js';
+
+
+import './css/App.css';
 
 function App() {
+  const [url, setUrl] = useState(false);
+
+  useEffect(() => {
+    setUrl(window.location.href);
+
+  }, [])
   return (
     <div className="App">
+      <h2 style={{ "color": 'red' }} >{url}</h2>
       <Header />
-      <Posts />
-    </div>
+      <div className="posts-wrapper">
+        <Router >
+
+          <Routes>
+            <Route path='/:year/:month/:day/:title' element={<Posts />} />
+
+          </Routes>
+        </Router>
+      </div>
+      <Footer />
+    </div >
   );
 }
 
