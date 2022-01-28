f1b37934f9cda7b1d57e608dab17f0b9bead687d
Justin Sitter <justin@jaytria.com>
2022-01-27 23:07:10 -0800

    Use file_get_contents

--- build/request.php
+++ build/request.php
@@ -1,5 +1,5 @@
 <?php
-$data = http_get("http://blog.jaytria.com/wp-json/wp/v2/posts/");
+$data = file_get_contents("http://blog.jaytria.com/wp-json/wp/v2/posts/");
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($data);
 ?>
