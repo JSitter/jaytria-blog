aaabae35394db5fc95072f3441951196093b4814
Justin Sitter <justin@jaytria.com>
2022-01-30 14:11:27 -0800

    Remove extra lines

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,9 @@
 const fetchJSON = (route) => {
     return new Promise((resolve, reject) => {
-
         try {
             const local_dev = '/wp-json/wp/v2/' + route;
             const prod = 'request.php'
 
-            // console.log(process.env.NODE_ENV === 'development' ? local_dev : prod)
             fetch(process.env.NODE_ENV === 'development' ? local_dev : prod)
                 .then(res => res.json())
                 .then(json => resolve(json))
