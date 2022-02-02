48b66615b8f0ee655a2cbb2e29caae0ea1cb69d8
Justin Sitter <justin@jaytria.com>
2022-02-02 14:55:23 -0800

    Hide debugging text

--- src/App.js
+++ src/App.js
@@ -4,29 +4,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header.js';
 import Posts from './Posts.js';
 import Footer from './Footer.js';
+import StickyMenu from './StickyMenu.js';
+import Intro from './Intro.js';
 
 
 import './css/App.css';
 
 function App() {
-  const [url, setUrl] = useState(false);
+  // const [url, setUrl] = useState(false);
 
-  useEffect(() => {
-    setUrl(window.location.href);
+  // useEffect(() => {
+  //   setUrl(window.location.href);
 
-  }, [])
+  // }, [])
   return (
     <div className="App">
-      <h2 style={{ "color": 'red' }} >{url}</h2>
+      {/* <h2 style={{ "color": 'red' }} >{url}</h2> */}
       <Header />
       <div className="posts-wrapper">
-        <Router >
-
-          <Routes>
-            <Route path='/:year/:month/:day/:title' element={<Posts />} />
-
-          </Routes>
-        </Router>
+        <StickyMenu />
+        <Posts />
       </div>
       <Footer />
     </div >
