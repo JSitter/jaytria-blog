9c5e52e69b439a6fee8f5b0a839aae3349c1a6e6
Justin Sitter <justin@jaytria.com>
2022-02-02 14:22:45 -0800

    Add Intro component

---
+++ src/Intro.js
@@ -0,0 +1,13 @@
+import './css/Intro.css';
+function Intro() {
+    return (
+        <div className="heading-intro-wrapper">
+            <h2>Designed and Developed in California</h2>
+            <p>
+                Documenting engineering successes for everyone.
+            </p>
+        </div>
+    );
+}
+
+export default Intro;
