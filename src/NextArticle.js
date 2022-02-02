2faa8bd2d9005601fc208424f4f705c138ff32e7
Justin Sitter <justin@jaytria.com>
2022-02-02 14:55:43 -0800

    Add NextArticle component

---
+++ src/NextArticle.js
@@ -0,0 +1,9 @@
+function NextArticle({ previous, next }) {
+    return (
+        <div>
+            <a href="#">Previous Article: Previous Article</a> | <a href="#">Next article: Article Title</a>
+        </div>
+    );
+}
+
+export default NextArticle;
