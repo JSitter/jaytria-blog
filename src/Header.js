ee0929e41071f67e2064e3d7b1b235d538faafe9
Justin Sitter <justin@jaytria.com>
2022-02-02 14:22:22 -0800

    Fix Header styling

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,20 @@
 import './css/Header.css';
+import Intro from './Intro.js';
 
 function Header() {
     return (
         <header>
-            <div className="header-background tilt">&nbsp</div>
-            <div className="header-background-stripe tilt">&nbsp</div>
+            <div className="header-stripes">
+                <div className="header-background tilt">&nbsp</div>
+                <div className="header-background-stripe tilt">&nbsp</div>
+                &nbsp
+            </div>
+
             <div className="website-heading">
                 <h1>Jaytria</h1>
-                <h2>Web Development</h2>
+                <h2>Web Development Blog</h2>
             </div>
+            <Intro />
         </header>
     );
 }
