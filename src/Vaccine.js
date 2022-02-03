5a0e761e17127e0cb04a189f6b443ec4af100173
Justin Sitter <justin@jaytria.com>
2022-02-03 01:20:26 -0800

    Add Vaccine component

---
+++ src/Vaccine.js
@@ -0,0 +1,9 @@
+const Vaccine = () => {
+    return (
+        <section>
+            &#xe510; <p>Find out where to <a href="https://www.vaccines.gov">get vaccinated</a> near you.</p>
+        </section>
+    );
+}
+
+export default Vaccine;
