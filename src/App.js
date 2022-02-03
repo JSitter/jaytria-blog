93669597d1de3e8fad099733a7e3555c9ca3e4ac
Justin Sitter <justin@jaytria.com>
2022-02-02 21:10:30 -0800

    Add 404 component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Header from './Header.js';
-import Posts from './Posts.js';
 import Footer from './Footer.js';
+import Header from './Header.js';
+import NotFound from './NotFound.js';
+import Posts from './Posts.js';
 import StickyMenu from './StickyMenu.js';
-import Intro from './Intro.js';
-
 
 import './css/App.css';
 
 function App() {
-  // const [url, setUrl] = useState(false);
 
-  // useEffect(() => {
-  //   setUrl(window.location.href);
-
-  // }, [])
   return (
     <div className="App">
-      {/* <h2 style={{ "color": 'red' }} >{url}</h2> */}
       <Header />
       <div className="posts-wrapper">
         <StickyMenu />
         <Router >
-
           <Routes>
             <Route path='/:year/:month/:day/:title' element={<Posts />} />
             <Route path='/' element={<Posts />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </Router>
       </div>
