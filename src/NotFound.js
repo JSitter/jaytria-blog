59faab2ea26c608c4a522f02d16fde92ea2fa58f
Justin Sitter <justin@jaytria.com>
2022-02-02 16:01:12 -0800

    Add NotFound component

---
+++ src/NotFound.js
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <section className="not-found-error">
+            <h2>???</h2>
+            <p><strong>Sorry</strong> but I couldn't find what you were looking for :'(</p>
+        </section>
+    );
+}
+
+export default NotFound;
