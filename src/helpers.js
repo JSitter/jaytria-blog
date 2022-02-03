1686205a83f10351a9387583265266a36c25d671
Justin Sitter <justin@jaytria.com>
2022-02-02 21:17:38 -0800

    Refactor fetchJSON

--- src/helpers.js
+++ src/helpers.js
@@ -2,12 +2,16 @@ const fetchJSON = (route) => {
     return new Promise((resolve, reject) => {
         try {
             const local_dev = `/wp-json/wp/v2/${route}`;
-            const prod = 'request.php'
+            const prod = 'https://blogtest.jaytria.com/request.php'
+
+            const location = process.env.NODE_ENV === 'development' ? local_dev : prod
 
             fetch(process.env.NODE_ENV === 'development' ? local_dev : prod)
                 .then(res => res.json())
                 .then(json => resolve(json))
-                .catch(err => reject(err));
+                .catch(err => {
+                    reject(err);
+                });
         } catch (err) {
             console.log(`Error fetching resource ${route}:  ${err}`);
         }
