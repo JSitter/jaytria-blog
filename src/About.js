72a157ca7fd344818f88e85738e31ca177afdb39
Justin Sitter <justin@jaytria.com>
2022-02-04 15:48:03 -0800

    Fix image location

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import './css/About.css';
 const About = () => {
     return (
         <section className="about-author">
-            <img className="selfie" src="selfie.JPG" />
+            <img className="selfie" src="/selfie.JPG" />
             <div>
                 <h2>About Justin</h2>
                 <p>Justin was born in Los Angeles and went to school in Santa Barbara and San Francisco. With more than a decade of experience in web development he now works out of Calaveras county after moving there to escape the pandemic.</p>
