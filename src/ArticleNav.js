ccc4d5beecea1f926da7f9f5a9612ffb5586b274
Justin Sitter <justin@jaytria.com>
2022-02-04 15:29:32 -0800

    Add ArticleNav component

--- src/ArticleNav.js
+++ src/ArticleNav.js
@@ -1,4 +1,32 @@
-function ArticleNav({ previous, next }) {
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
+function convertCurrentPostToIndex(posts, params) {
+    console.log(params);
+    console.log(posts)
+    if ("title" in params) {
+        const index = posts.some((post, index) => {
+            console.log(post)
+            // if (post.link)
+        })
+    }
+}
+
+function createPreviousPostLink(curIndex) {
+
+}
+
+function createNextPostLink(curIndex) {
+
+}
+
+function ArticleNav({ posts }) {
+    let params = useParams();
+
+    useEffect(() => {
+        convertCurrentPostToIndex(posts, params)
+    }, [params])
+
     return (
         <div>
             <a href="#">Previous Article: Previous Article</a> | <a href="#">Next article: Article Title</a>
