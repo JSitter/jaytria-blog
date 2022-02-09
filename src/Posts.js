0115ddaac4e676c83590b1404e2a2fe2de664dcc
Justin Sitter <justin@jaytria.com>
2022-02-09 00:10:04 -0800

    Refactor sharing link into separate component

--- src/Posts.js
+++ src/Posts.js
@@ -18,11 +18,10 @@ const LoadingSpinner = () => {
     );
 }
 
-const Article = (post) => {
+const ShareArticleLink = ({ post }) => {
     const [showSharingUrl, setShowSharingUrl] = useState(false);
     const [clipboardCopied, setClipboardCopied] = useState(false);
-    // const shareLink = `${window.location.host}/${post.response.link.split('/').slice(-5, -1).join('/')}`;
-    const shareLink = window.location.href;
+    const shareLink = `${window.location.host}/${post.response.link.split('/').slice(-5, -1).join('/')}`;
 
     const copied = (text) => {
         copyToClipboard(text)
@@ -43,39 +42,48 @@ const Article = (post) => {
     }
 
     return (
+        <section className="share-article">
+            <div
+                onClick={() => setShowSharingUrl(!showSharingUrl)}
+                className="share-icon"
+                tabIndex="0"
+                onKeyDown={sendLinkFocus}
+            >
+                <FontAwesomeIcon
+                    className="share-icon fa-3x"
+                    icon={faPaperPlane}
+                />
+                <small>Share</small>
+            </div>
+            <div className={`share-url ${!showSharingUrl ? 'hide' : 'visible'}`}>
+                <div className="share-title-bar"><h4 dangerouslySetInnerHTML={{ __html: post.response.title.rendered }}></h4></div>
+                <input onFocus={() => copied(shareLink)} value={shareLink} readOnly />
+                <div className="link-buttons">
+                    <span className="link-button" onClick={() => copied(shareLink)}>Copy Link</span>
+                    <span className="link-button" onClick={() => setShowSharingUrl(false)} >Close</span>
+                </div>
+                <span className="close" tabIndex="0" onKeyDown={closeSendLinkFocus} onClick={() => setShowSharingUrl(false)}>
+                    <FontAwesomeIcon
+                        className="close fa-2x"
+                        icon={faTimesCircle}
+                    />
+                </span>
+                <div className={`copied-location ${clipboardCopied ? "visible" : "hide"}`}>Copied to Clipboard</div>
+            </div>
+        </section>
+    );
+}
+
+const Article = (post) => {
+
+    // const shareLink = `${window.location.host}/${post.response.link.split('/').slice(-5, -1).join('/')}`;
+
+
+    return (
         <article>
             <h2 dangerouslySetInnerHTML={{ __html: post.response.title.rendered }}></h2>
             <section dangerouslySetInnerHTML={{ __html: post.response.content.rendered }}></section>
-
-            <section className="share-article">
-                <div
-                    onClick={() => setShowSharingUrl(!showSharingUrl)}
-                    className="share-icon"
-                    tabIndex="0"
-                    onKeyDown={sendLinkFocus}
-                >
-                    <FontAwesomeIcon
-                        className="share-icon fa-3x"
-                        icon={faPaperPlane}
-                    />
-                    <small>Share</small>
-                </div>
-                <div className={`share-url ${!showSharingUrl ? 'hide' : 'visible'}`}>
-                    <div className="share-title-bar"><h4 dangerouslySetInnerHTML={{ __html: post.response.title.rendered }}></h4></div>
-                    <input onFocus={() => copied(shareLink)} value={shareLink} readOnly />
-                    <div className="link-buttons">
-                        <span className="link-button" onClick={() => copied(shareLink)}>Copy Link</span>
-                        <span className="link-button" onClick={() => setShowSharingUrl(false)} >Close</span>
-                    </div>
-                    <span className="close" tabIndex="0" onKeyDown={closeSendLinkFocus} onClick={() => setShowSharingUrl(false)}>
-                        <FontAwesomeIcon
-                            className="close fa-2x"
-                            icon={faTimesCircle}
-                        />
-                    </span>
-                    <div className={`copied-location ${clipboardCopied ? "visible" : "hide"}`}>Copied to Clipboard</div>
-                </div>
-            </section>
+            <ShareArticleLink post={post} />
         </article >
     );
 }
