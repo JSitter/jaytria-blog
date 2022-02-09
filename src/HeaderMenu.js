64a3851b1456c451944902f763bf825e94faa61f
Justin Sitter <justin@jaytria.com>
2022-02-08 17:57:33 -0800

    Add HeaderMenu component

--- src/HeaderMenu.js
+++ src/HeaderMenu.js
@@ -1,10 +1,12 @@
 function HeaderMenu({ links }) {
     return (
-        <ul>
-            {links.map((link) => {
-                return (<li><a href={link.url}>{link.title}</a></li>);
-            })}
-        </ul>
+        <nav>
+            <ul>
+                {links.map((link) => {
+                    return (<li><a href={link.url}>{link.title}</a></li>);
+                })}
+            </ul>
+        </nav>
     );
 }
 
